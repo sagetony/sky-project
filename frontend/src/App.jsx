1f2c69c25ded1f5ba28008294625057d0c548243
Sagetony <uchetony224@gmail.com>
2024-12-13 15:16:53 +0100

    added nft minting frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
+import MintingForm from "./components/MintingForm";
 import Home from "./pages/Home";
 import Maps from "./pages/Map";
 import Login from "./pages/auth/Login";
@@ -56,6 +57,7 @@ function App() {
         <Route path="/staking" element={<Staking />} />
         <Route path="/city-fund" element={<CityFund />} />
         <Route path="/about" element={<About />} />
+        <Route path="/minting" element={<MintingForm />} />
       </Routes>
       <Footer />
     </Layout>
