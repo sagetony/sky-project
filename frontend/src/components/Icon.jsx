603e7d03a90145b252aa2bfc5915b8cb2a5919e6
Sheila-a <sheila.codes.m@gmail.com>
2024-12-20 12:11:30 +0100

    feat: Profile endpoint implementation

--- frontend/src/components/Icon.jsx
+++ frontend/src/components/Icon.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable react/prop-types */
 import { Icon } from '@iconify/react';
-const Icons = ({ icon, className, width, fill, rotate, hFlip, vFlip }) => {
+const Icons = ({
+  icon,
+  className,
+  width,
+  fill,
+  rotate,
+  hFlip,
+  vFlip,
+  onClick,
+}) => {
   return (
     <>
       <Icon
+        onClick={onClick}
+        //
         // data-aos='zoom-in'
         // data-aos-duration='1000'
         width={width}
